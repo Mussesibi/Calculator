f4f55f269ed4176c6c1204662e218a4146ab13cc
Salem <109506748+Mussesibi@users.noreply.github.com>
2024-02-15 18:15:53 +0530

    Finish logic of calculator This will make the calculator full of required functionalities

--- script.js
+++ script.js
@@ -1,16 +1,114 @@
 const buttons = document.querySelectorAll("button")
 const display = document.querySelector(".display-portal")
+
+let currentInput = ""
+let currentOperator = ""
+let storedValue = 0 
+
 buttons.forEach(btn => {
     btn.addEventListener('click', ()=>{
-        if(btn.value === ""){
-            display.textContent = ""
-        } else{
 
-            const value = btn.value
-            display.textContent += value
+        const value = btn.value
+        // If C button is clicked will erase all data
+        if(value === ""){
+            display.textContent = ""
+            storedValue = 0
+            currentInput = ""
+            currentOperator = ""
+
+        } else if (value === "="){
+            handleEqual();
+
+        } else if(value === "+" ||value === "-"||value === "*"||value === "/"){
+            if(currentInput !== "" && currentOperator){
+                handleEqual();
+                
+            }
+            handleOperator(value)
+            
+        }else if (value === "."){
+            handleDecimal()
         }
-        btn.style.cssText = "-webkit-box-shadow: inset -8px -7px 5px -9px rgba(255,255,255,1);"
-        "-moz-box-shadow: inset -8px -7px 5px -9px rgba(255,255,255,1);"
-        "box-shadow: inset -8px -7px 5px -9px rgba(255,255,255,1);"
+        else{
+            currentInput += value
+            updateDisplay()            
+        }
+        applyBtnStyle(btn)
     })
 })
+
+// Functions for calculations
+function add(a, b){return a+b}
+
+function sub(a, b){return a-b}
+
+function mul(a, b){
+    let result = a * b
+    result = result.toFixed(8)
+    return result
+}
+function divide(a, b){
+    if (b === 0) {
+        return display.textContent = "You naughty boy ;)"
+    }else{
+
+        let result = a / b
+        result = result.toFixed(8)
+        return result
+    }
+}
+
+
+// Will check the and update the operators and store values in a variable 
+// Then it'll free currentInput to accept another input 
+function handleOperator(operator){
+    if(currentInput !==""){
+        storedValue = parseFloat(currentInput)
+        display.textContent = ""
+        currentOperator = operator
+        currentInput = ""
+        updateDisplay();
+        
+    }else{
+        currentOperator = operator
+    }
+}
+// Will not allow adding decimal point if already exists one (1.2.3)NOTALLOWED
+function handleDecimal(){
+    if(!currentInput.includes(".")){
+        currentInput += "."
+        updateDisplay()
+    }
+}
+
+// Will Do the calculations and handles some stuff
+function handleEqual(){
+    if(!currentOperator){
+        display.textContent = "Fuck you "
+    }
+    else if(currentInput !== ""){
+        const operand = parseFloat(currentInput)
+        switch(currentOperator){
+            case "+" : storedValue = add(storedValue,operand)
+            break;
+            case "-" : storedValue = sub(storedValue,operand)
+            break;
+            case "*" : storedValue = mul(storedValue,operand)
+            break;
+            case "/" : storedValue = divide(storedValue,operand)
+            break;
+        }
+        currentInput = ""
+        updateDisplay();
+    }else{
+
+    }
+}
+
+function applyBtnStyle(btn){
+    btn.classList.add('active')
+}
+function updateDisplay(){
+    display.textContent = currentInput !== "" ? currentInput : storedValue;
+
+}
